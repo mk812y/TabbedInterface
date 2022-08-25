1941c757cc0b159f8697347bb5cc9d4354c361fb
mk812y <mk812y@gmail.com>
2022-08-25 17:30:02 +0300

    Initial Commit

---
+++ TabbedInterface/TabbedInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbedInterfaceApp.swift
+//  TabbedInterface
+//
+//  Created by Михаил Куприянов on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TabbedInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
